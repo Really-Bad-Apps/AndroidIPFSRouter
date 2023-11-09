fad7f2fbd39950496e03bab3a26a01174c2af6f5
Jason Hudgins <jason@hudgins.co>
2023-11-09 13:04:39 +0000

    implemented some basic unit tests (more to do) and got all the dependencies fixed so they pass

--- library/src/androidTest/java/com/reallybadapps/ipfsrouter/IpfsGatewayUtilsInstrumentedTest.kt
+++ library/src/androidTest/java/com/reallybadapps/ipfsrouter/IpfsGatewayUtilsInstrumentedTest.kt
@@ -2,7 +2,9 @@ package com.reallybadapps.ipfsrouter
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.runBlocking
 
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -14,11 +16,24 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class IpfsGatewayUtilsInstrumentedTest {
+
+    @Before
+    fun setUp() = runBlocking {
+        // runBlocking will block the thread until the coroutine inside it is completed
+        nodeCheck() // Assuming nodecheck is a suspend function
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.reallybadapps.ipfsrouter.test", appContext.packageName)
+    }
+
+    @Test
+    fun testGetFastestNode() {
+        val fastestNode = getFastestNode()
+        assertNotNull( fastestNode)
     }
 }
