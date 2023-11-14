7b5125ec0853932fed6260a98497b94799490acd
Jason Hudgins <jason@hudgins.co>
2023-11-14 16:56:26 +0000

    added missing unit tests

--- library/src/androidTest/java/com/reallybadapps/ipfsrouter/IpfsGatewayUtilsInstrumentedTest.kt
+++ library/src/androidTest/java/com/reallybadapps/ipfsrouter/IpfsGatewayUtilsInstrumentedTest.kt
@@ -25,6 +25,25 @@ class IpfsGatewayUtilsInstrumentedTest {
     }
 
     @Test
+    fun testUrlTransformation() {
+        val node = Node(host = "example.com", remote = true)
+        val transformedUrl = transform("ipfs://QmPChd2hVbrJ6bfo3WBcTW4iZnpHm8TEzWkLHmLpXhF68A", node)
+        assertEquals("https://example.com/ipfs/QmPChd2hVbrJ6bfo3WBcTW4iZnpHm8TEzWkLHmLpXhF68A", transformedUrl)
+    }
+
+    @Test
+    fun testCidValidation() {
+        assertTrue(isCID("QmPChd2hVbrJ6bfo3WBcTW4iZnpHm8TEzWkLHmLpXhF68A"))
+        assertFalse(isCID("invalidCid"))
+    }
+
+    @Test
+    fun testNodeCheck() = runBlocking {
+        nodeCheck()
+        assertTrue(nodeList.isNotEmpty())
+    }
+
+    @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
@@ -35,5 +54,39 @@ class IpfsGatewayUtilsInstrumentedTest {
     fun testGetFastestNode() {
         val fastestNode = getFastestNode()
         assertNotNull( fastestNode)
+    }
+
+    @Test
+    fun testGetPreferredNode() {
+        // Set up a preferred gateway
+        setPreferredGateway(InstrumentationRegistry.getInstrumentation().targetContext, "auto")
+        // Call getPreferredNode and assert the result
+        val preferredNode = getPreferredNode(InstrumentationRegistry.getInstrumentation().targetContext)
+        assertNotNull(preferredNode)
+    }
+
+    @Test
+    fun testIsCID_withValidCID_returnsTrue() {
+        val validCID = "QmPChd2hVbrJ6bfo3WBcTW4iZnpHm8TEzWkLHmLpXhF68A"
+        assertTrue(isCID(validCID))
+    }
+
+    @Test
+    fun testIsCID_withInvalidCID_returnsFalse() {
+        val invalidCID = "NotACID"
+        assertFalse(isCID(invalidCID))
+    }
+
+    @Test
+    fun testIsBase32EncodedMultibase_withBase32CID_returnsTrue() {
+        // Example CID in Base32 Encoding (you might need to replace this with an actual Base32 encoded CID)
+        val base32CID = "bafybeiam2jkwnk42kyvgce33hekrmtr2bkmoozx5ttw66lwljizv2josam"
+        assertTrue(isBase32EncodedMultibase(base32CID))
+    }
+
+    @Test
+    fun testIsBase32EncodedMultibase_withNonBase32CID_returnsFalse() {
+        val nonBase32CID = "QmPChd2hVbrJ6bfo3WBcTW4iZnpHm8TEzWkLHmLpXhF68A" // Regular CID
+        assertFalse(isBase32EncodedMultibase(nonBase32CID))
     }
 }
