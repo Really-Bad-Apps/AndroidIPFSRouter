95db9816e5c31bea0f1ea1be4d1a27dd3de9961e
Jason Hudgins <jason@hudgins.co>
2023-11-16 12:53:47 +0000

    refactored project so the library is called AndroidIPFSLibrary and not just library

---
+++ AndroidIPFSRouter/src/test/java/com/reallybadapps/ipfsrouter/IpfsGatewayUtilsTest.kt
@@ -0,0 +1,13 @@
+package com.reallybadapps.ipfsrouter
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+// add unit tests for IpfsGatewayUtils.it
+class IpfsGatewayUtilsTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
